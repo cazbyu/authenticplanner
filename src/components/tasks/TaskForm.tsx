fe39ccc60cfed03f64343e44184bf4dd4b701194
cazbyu <131417207+cazbyu@users.noreply.github.com>
2025-07-15 19:11:03 -0600

    Updated TaskForm.tsx

--- src/components/tasks/TaskForm.tsx
+++ src/components/tasks/TaskForm.tsx
@@ -557,7 +557,7 @@ function handle12WeekGoalToggle() {
                     }));
                   }}
                   disabled={form.isAllDay}
-                  className="w-24 text-sm border border-gray-300 rounded-md px-3 py-2 bg-gray-50 focus:outline-none focus:ring-1 focus:ring-blue-500 disabled:bg-gray-100 appearance-none"
+                  className="w-24 text-xs border border-gray-300 rounded-md px-3 py-2 bg-gray-50 focus:outline-none focus:ring-1 focus:ring-blue-500 disabled:bg-gray-100 appearance-none"
                 >
                   {timeOptions.map(time => (
                     <option key={time.value} value={time.value}>{time.label}</option>
