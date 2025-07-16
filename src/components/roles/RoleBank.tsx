8a0b25a1cb23b527ed5910e2e6a7b1090b3987e0
cazbyu <131417207+cazbyu@users.noreply.github.com>
2025-07-15 21:50:45 -0600

    Updated RoleBank.tsx

--- src/components/roles/RoleBank.tsx
+++ src/components/roles/RoleBank.tsx
@@ -550,11 +550,7 @@ const RoleBank: React.FC<RoleBankProps> = ({ selectedRole: propSelectedRole, onB
               </button>
             ))
           )}
-          {/* -- ADD THIS BLOCK BELOW -- */}
-  {Array.from({ length: 30 }).map((_, i) => (
-    <div key={`dummy-${i}`} className="p-4 border border-dashed border-blue-400 rounded-lg bg-blue-50 text-blue-800">
-      Dummy Role {i + 1}
-    </div>
+         
   ))}
 </div>
         </div>
@@ -562,7 +558,6 @@ const RoleBank: React.FC<RoleBankProps> = ({ selectedRole: propSelectedRole, onB
     </div>
   );
 }
-
 
   // --- MAIN LANDING VIEW (three feature boxes and header) ---
   return (
