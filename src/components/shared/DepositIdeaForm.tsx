6ba70087cbd97173e3e1a11ccaa28dc95d17c1ee
cazbyu <131417207+cazbyu@users.noreply.github.com>
2025-07-19 10:56:56 -0600

    Updated DepositIdeaForm.tsx

--- src/components/shared/DepositIdeaForm.tsx
+++ src/components/shared/DepositIdeaForm.tsx
@@ -1,5 +1,5 @@
 import React, { useState, useEffect } from 'react';
-import { supabase } from '../supabaseClient';
+import { supabase } from '../../supabaseClient';
 import { toast } from 'sonner';
 import { Users, Heart } from 'lucide-react';
 
