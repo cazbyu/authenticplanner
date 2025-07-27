15a0d108922d5f4bc99dc4cd825dfcca684966ae
cazbyu <131417207+cazbyu@users.noreply.github.com>
2025-07-27 17:09:24 -0600

    Updated UnscheduledPriorities.tsx

--- src/components/tasks/UnscheduledPriorities.tsx
+++ src/components/tasks/UnscheduledPriorities.tsx
@@ -298,7 +298,7 @@ const UnscheduledPriorities: React.FC<UnscheduledPrioritiesProps> = ({ tasks, se
               {/* Header - Always visible, compact when collapsed */}
               <button 
                 className={`w-full ${bgColor} ${textColor} px-3 py-2 rounded-lg flex-shrink-0 hover:opacity-90 transition-opacity focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-blue-500`}
-                onClick={() => ontoggleQuadrant(id)}
+                onClick={() => onToggleQuadrant(id)}
                 type="button"
               >
                 <div className="flex items-center justify-between">
@@ -422,8 +422,7 @@ const UnscheduledPriorities: React.FC<UnscheduledPrioritiesProps> = ({ tasks, se
           </div>
         )}
       </div>
-    </DragDropContext>
-  );
+     );
 };
 
 export default UnscheduledPriorities;
