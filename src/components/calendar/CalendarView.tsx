e322f10e2a89e8ba2820d7c4c0704c3c1c02d189
cazbyu <131417207+cazbyu@users.noreply.github.com>
2025-07-28 10:25:28 -0600

    Updated CalendarView.tsx

--- src/components/calendar/CalendarView.tsx
+++ src/components/calendar/CalendarView.tsx
@@ -198,62 +198,49 @@ const CalendarView = forwardRef<FullCalendar, CalendarViewProps>(
           height="100%"
           events={events}
           editable={true}
-          eventStartEditable={true}
-          eventResizableFromStart={false}
-          eventDurationEditable={true}
           droppable={true}
           selectable={true}
           selectMirror={true}
-          dropAccept="*"
           dayMaxEvents={true}
           weekends={true}
-          eventOverlap={true}
-          selectOverlap={true}
           eventClick={handleEventClick}
           drop={handleDrop}
           eventDrop={handleEventChange}
           eventResize={handleEventChange}
-          eventAllow={(dropInfo, draggedEvent) => {
-            // Allow all event movements
-            return true;
-          }}
-          eventMouseEnter={(info) => {
-            info.el.style.cursor = 'move';
-            info.el.style.zIndex = '999';
-          }}
-          eventMouseLeave={(info) => {
-            info.el.style.cursor = 'default';
-            info.el.style.zIndex = 'auto';
-          }}
           dayMinTime="00:00:00"
           dayMaxTime="24:00:00"
           allDaySlot={true}
           nowIndicator={true}
           slotDuration="00:30:00"
           slotLabelInterval="01:00"
-          expandRows={true} // Changed back to true
-          stickyHeaderDates={false}
-          scrollTime="06:00:00"
-          scrollTimeReset={false}
-          slotLabelFormat={{ hour: 'numeric', minute: '2-digit', omitZeroMinute: true, meridiem: 'short' }}
+          expandRows={true}
+          stickyHeaderDates={true}
+          slotLabelFormat={{
+            hour: 'numeric',
+            minute: '2-digit',
+            omitZeroMinute: true,
+            meridiem: 'short',
+          }}
           views={{
-            dayGridMonth: { firstDay: 0, fixedWeekCount: false, showNonCurrentDates: true },
-            timeGridWeek: { 
-              firstDay: 0, 
-              slotDuration: '00:15:00', 
-              slotLabelInterval: '01:00', 
-              allDaySlot: true, 
-              expandRows: true, // Changed back to true
-              scrollTime: '06:00:00'
+            dayGridMonth: {
+              firstDay: 0,
+              fixedWeekCount: false,
+              showNonCurrentDates: true,
             },
-            timeGridDay: { 
-              dayCount: 1, 
-              firstDay: 0, 
-              slotDuration: '00:15:00', 
-              slotLabelInterval: '01:00', 
-              allDaySlot: true, 
-              expandRows: true, // Changed back to true
-              scrollTime: '06:00:00'
+            timeGridWeek: {
+              firstDay: 0,
+              slotDuration: '00:15:00',
+              slotLabelInterval: '01:00',
+              allDaySlot: true,
+              expandRows: true,
+            },
+            timeGridDay: {
+              dayCount: 1,
+              firstDay: 0,
+              slotDuration: '00:15:00',
+              slotLabelInterval: '01:00',
+              allDaySlot: true,
+              expandRows: true,
             },
           }}
           datesSet={handleDatesSet}
