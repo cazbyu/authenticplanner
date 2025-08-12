a4d752a5dbf7fee9d340958548f6db96fb4ef59d
cazbyu <131417207+cazbyu@users.noreply.github.com>
2025-08-11 22:43:55 -0600

    Updated MainLayout.tsx

--- src/layouts/MainLayout.tsx
+++ src/layouts/MainLayout.tsx
@@ -1,8 +1,9 @@
 // MainLayout.tsx
 import React, { useState } from 'react';
 import { Outlet, Link, useLocation } from 'react-router-dom';
-import { Menu, Home, Calendar, Settings, Users, Compass, Clock,
-  Target, BookOpen, BarChart3, Briefcase, Archive, ChevronRight
+import { 
+  Menu, Home, Calendar, Settings, Users, Compass, Clock,
+  Target, BookOpen, BarChart3, Briefcase, Archive, ChevronRight, X, LogOut, Drama as Drawer 
 } from 'lucide-react';
 import { motion, AnimatePresence } from 'framer-motion';
 import logo from '../assets/logo.svg';
